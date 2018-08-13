27eb7a348849a971a21784e53cb405a626540a78
jgurowsky@gmail.com <jgurowsky@gmail.com>
2018-08-13 05:20:04 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -4,7 +4,7 @@ class Owner
   attr_accessor :name
   attr_reader :species
 
-  def initialize(species, name)
+  def initialize(species)
     @name = name
     @species = species
     @@owners << self
